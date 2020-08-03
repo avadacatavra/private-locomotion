1fa2eab3d747b3154c3a24b65ddc66ebf24afc14
avadacatavra <avadacatavra@gmail.com>
2020-08-03 12:56:02 -0400

    added user column to data for concatenation

--- classification.py
+++ classification.py
@@ -3,13 +3,16 @@ import pandas as pd
 import numpy as np
 from torch.utils.data import Dataset
 
+# each dataset contains the data for a single person, so we'll need to concat datasets
+# that means we should create a new column containing the user identifier
 class PosesDataset(Dataset):
     def __init__(self, csv_file, transform=None):
         self.poses_frame = pd.read_csv(csv_file)
         self.transform = transform
 
         # parse user from csv_file name
-        self.user = csv_file.split('_')[3]
+        user = csv_file.split('_')[3]
+        self.poses_frame['user'] = [user] * len(self.poses_frame)
 
     def __len__(self):
         return len(self.poses_frame)
@@ -18,10 +21,11 @@ class PosesDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        poses = self.poses_frame.iloc[idx, 1:]
+        user = self.poses_frame.iloc[idx]['user']
+        poses = self.poses_frame.iloc[idx, 1:-1]
         poses = np.array([poses])
 
-        sample = {'user': self.user, 'poses': poses}
+        sample = {'user': user, 'poses': poses}
         
         return sample
 
@@ -32,7 +36,9 @@ class ThrowsDataset(Dataset):
         self.transform = transform
 
         # parse user from csv_file name
-        self.user = poses_file.split('_')[3]
+        user = poses_file.split('_')[3]
+        self.poses_frame['user'] = [user] * len(self.poses_frame)
+        
 
         grabs = self.events_frame[self.events_frame['event'] == 'GRABBED_OBJECT']
         throws = self.events_frame[self.events_frame['event'] == 'TARGET_THROW']
@@ -59,10 +65,11 @@ class ThrowsDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
-        poses = self.throws_poses.iloc[idx, 1:]
+        user = self.poses_frame.iloc[idx]['user']
+        poses = self.throws_poses.iloc[idx, 1:-1]
         poses = np.array([poses])
 
-        sample = {'user': self.user, 'throw pose': poses}
+        sample = {'user': user, 'throw pose': poses}
 
 
 
@@ -77,8 +84,7 @@ class ToTensor(object):
 
 
 pose_data = PosesDataset(csv_file='data/1_Teleport_0_10_poses.csv')
-
-#print(len(pose_data))
+print(pose_data[0])
 
 events_file = 'data/1_HandWalking_0_133_1596455970log/1_HandWalking_0_133_events.csv'
 poses_file = 'data/1_HandWalking_0_133_1596455970log/1_HandWalking_0_133_poses.csv'
