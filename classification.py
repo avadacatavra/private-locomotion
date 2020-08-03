3ffd1593dc60fbb1d7e55987ab94b9faafd5f861
avadacatavra <avadacatavra@gmail.com>
2020-08-03 12:29:22 -0400

    added throws dataset

--- classification.py
+++ classification.py
@@ -25,6 +25,47 @@ class PosesDataset(Dataset):
         
         return sample
 
+class ThrowsDataset(Dataset):
+    def __init__(self, poses_file, events_file, transform=None):
+        self.poses_frame = pd.read_csv(poses_file)
+        self.events_frame = pd.read_csv(events_file)
+        self.transform = transform
+
+        # parse user from csv_file name
+        self.user = poses_file.split('_')[3]
+
+        grabs = self.events_frame[self.events_frame['event'] == 'GRABBED_OBJECT']
+        throws = self.events_frame[self.events_frame['event'] == 'TARGET_THROW']
+        # for the time between a grab and a release, get the poses
+        starttimes = []
+        for _, grab in grabs.iterrows():
+            starttimes.append(grab['time'])
+
+        endtimes = []
+        for _, grab in throws.iterrows():
+            endtimes.append(grab['time'])
+
+        times = zip(starttimes, endtimes)
+
+        self.throws_poses = pd.DataFrame()
+        for (start, end) in times:
+            throw = self.poses_frame[(self.poses_frame['time'] > start) & (self.poses_frame['time'] < end)]
+            self.throws_poses = self.throws_poses.append(throw, ignore_index = True)
+
+    def __len__(self):
+        return len(self.throws_poses)
+
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+
+        poses = self.throws_poses.iloc[idx, 1:]
+        poses = np.array([poses])
+
+        sample = {'user': self.user, 'throw pose': poses}
+
+
+
 class ToTensor(object):
     """ convert ndarrays to Tensors """
 
@@ -37,6 +78,10 @@ class ToTensor(object):
 
 pose_data = PosesDataset(csv_file='data/1_Teleport_0_10_poses.csv')
 
-print(len(pose_data))
+#print(len(pose_data))
 
+events_file = 'data/1_HandWalking_0_133_1596455970log/1_HandWalking_0_133_events.csv'
+poses_file = 'data/1_HandWalking_0_133_1596455970log/1_HandWalking_0_133_poses.csv'
+
+throws_data = ThrowsDataset(poses_file=poses_file, events_file=events_file)
 
