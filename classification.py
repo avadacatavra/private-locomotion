3ea889ba701a7b2fb59efbfa6b2081097899b4c7
avadacatavra <avadacatavra@gmail.com>
2020-08-03 11:55:42 -0400

    added totensor

--- classification.py
+++ classification.py
@@ -4,8 +4,12 @@ import numpy as np
 from torch.utils.data import Dataset
 
 class PosesDataset(Dataset):
-    def __init__(self, csv_file):
+    def __init__(self, csv_file, transform=None):
         self.poses_frame = pd.read_csv(csv_file)
+        self.transform = transform
+
+        # parse user from csv_file name
+        self.user = csv_file.split('_')[3]
 
     def __len__(self):
         return len(self.poses_frame)
@@ -17,9 +21,18 @@ class PosesDataset(Dataset):
         poses = self.poses_frame.iloc[idx, 1:]
         poses = np.array([poses])
 
-        sample = {'time': self.poses_frame.iloc[idx, 0], 'poses': poses}
+        sample = {'user': self.user, 'poses': poses}
         
         return sample
+
+class ToTensor(object):
+    """ convert ndarrays to Tensors """
+
+    def __call__(self, sample):
+        user, poses = sample['user'], sample['poses']
+
+        return {'user': torch.from_numpy(user), 'poses': torch.from_numpy(poses)}
+        
 
 
 pose_data = PosesDataset(csv_file='data/1_Teleport_0_10_poses.csv')
